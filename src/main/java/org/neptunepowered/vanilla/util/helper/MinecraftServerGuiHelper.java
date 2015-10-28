80b3b1d41ae43ecf27eada998268bd7e5be2bde0
Jamie Mansfield <malgm@lapis.blue>
2015-10-28 20:08:19 +0000

    Brand server gui

---
+++ src/main/java/org/neptunepowered/vanilla/util/helper/MinecraftServerGuiHelper.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of NeptuneVanilla, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Jamie Mansfield <https://github.com/jamierocks>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.neptunepowered.vanilla.util.helper;
+
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.gui.MinecraftServerGui;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class MinecraftServerGuiHelper {
+
+    public static JFrame createServerGui(final DedicatedServer serverIn) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception exception) {
+        }
+
+        MinecraftServerGui minecraftservergui = new MinecraftServerGui(serverIn);
+        JFrame jframe = new JFrame("NeptuneVanilla - Lightweight CanaryLib implementation");
+        jframe.add(minecraftservergui);
+        jframe.pack();
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+        jframe.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent p_windowClosing_1_) {
+                serverIn.initiateShutdown();
+
+                while (!serverIn.isServerStopped()) {
+                    try {
+                        Thread.sleep(100L);
+                    } catch (InterruptedException interruptedexception) {
+                        interruptedexception.printStackTrace();
+                    }
+                }
+
+                System.exit(0);
+            }
+        });
+        return jframe;
+    }
+}
